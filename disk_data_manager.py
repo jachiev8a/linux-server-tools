445b85c5e9b2e65a649a5b5e9c6da58c213169a2
Javier Ochoa <javier.ochoa@osram-continental.com>
2021-01-06 18:59:15 -0600

    added improvement to parse the csv file to get the data from (FIX)

--- disk_data_manager.py
+++ disk_data_manager.py
@@ -7,7 +7,7 @@ def get_labels():
         csv_reader = csv.reader(file_obj, delimiter=',')
         for row in csv_reader:
             labels_list = str(row).split(',')
-            print("labels: ".format(labels_list))
+            print("labels: {}".format(labels_list))
             break
 
 
@@ -21,7 +21,7 @@ def get_values():
                 line_count += 1
             else:
                 int_value = float(row[2].replace('M', '').replace('G', ''))
-                print("val: ".format(int_value))
+                print("val: {}".format(int_value))
                 value_list.append(int_value)
                 line_count += 1
 
@@ -35,5 +35,5 @@ def get_max_value():
                 line_count += 1
             else:
                 int_value = float(row[1].replace('M', '').replace('G', ''))
-                print("MAX: ".format(int_value))
+                print("MAX: {}".format(int_value))
                 return int_value
