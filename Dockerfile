c9a39645c94cec7264866e600d4035c1efc5ce0c
Javier Ochoa <javier.ochoa@osram-continental.com>
2021-01-07 21:19:44 -0600

    added docker support to run flask app

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.6
+
+WORKDIR /app
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ["./requirements.txt", "/app/requirements.txt"]
+
+RUN pip3 install --upgrade pip && \
+    pip3 install -r requirements.txt
+
+COPY [".", "/app/"]
+
+EXPOSE 3000
+
+# run server to be seen from outside
+# ----------------------------------------------------------------------
+CMD ["flask", "run", "--host=0.0.0.0"]
---
+++ docker-start-app.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# import other libraries
+source ./utils/logging_utils.sh
+
+# ----------------------------------------------------------------------
+# Script definitions
+# ----------------------------------------------------------------------
+
+# default one used as repo path to deploy
+OS_MONITOR_OUTPUT=/opt/linux-server-tools/os-monitor/out
+
+# usage help use
+# ----------------------------------------------------------------------
+usage() {
+    log_info "\n--- [DOCKER]: docker-start-app.sh ---\n"
+    echo -e "Usage:\n"
+    exit 0
+}
+
+# script error handler
+# ----------------------------------------------------------------------
+handle_error() {
+    error_msg=$1
+    log_error ""
+    log_error "==================================================================="
+    log_error " > [DOCKER]: ERROR:"
+    log_error " > $error_msg"
+    log_error "==================================================================="
+    log_warning "\n > Exiting...\n"
+    exit 1
+}
+
+# run script as root
+# ----------------------------------------------------------------------
+if [ "$EUID" -ne 0 ] ; then
+    handle_error "Please run this script as 'root'"
+    exit 1
+fi
+
+# validate arguments parsing
+# ----------------------------------------------------------------------
+while getopts "h" option; do
+    case "$option" in
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+echo "" # new line
+
+echo " > [DOCKER]: Running Docker App as user: '$(whoami)'"
+
+# create logs folder (if not exists)
+# ----------------------------------------------------------------------
+if [ ! -d "./logs" ] ; then
+    echo " > [DOCKER]: Creating logs folder..."
+    mkdir logs
+fi
+
+# validate output monitor directory exists
+# ----------------------------------------------------------------------
+if [ ! -d "$OS_MONITOR_OUTPUT" ] ; then
+    handle_error "'$OS_MONITOR_OUTPUT' is not created yet!"
+fi
+
+# set the repo path variable use at docker-compose file.
+export OS_MONITOR_OUTPUT="$OS_MONITOR_OUTPUT"
+
+log_debug " > [DOCKER]: Executing docker-compose Process...\n"
+
+echo -e " > [DOCKER]: Stop all running containers..."
+docker-compose -f docker-compose.yml down
+log_info " > [DOCKER]: Containers Stopped [OK]\n"
+echo -e " ------------------------------------------------------------"
+
+echo -e " > [DOCKER]: Starting containers..."
+docker-compose -f docker-compose.yml up --build -d
+docker_exit_status=$?
+
+if [ $docker_exit_status -ne 0 ]; then
+    handle_error "docker-compose command failed! Check the logs..."
+fi
+
+echo -e "\n"
+log_info "==================================================================="
+log_info " > [DOCKER]: Docker App Executed! [OK]"
+log_info ""
+log_info " > [Executed in Background...]"
+log_info "==================================================================="
+echo -e "\n"
