8c5575b0415e556f3b900f9b7d13fb7c7d17b12b
Javier Ochoa <javier.ochoa@osram-continental.com>
2021-01-14 18:15:31 -0600

    added changes on bootstrap improvements (margins). added disk manager class and singleton to manage all disks (FIX)

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ import argparse
 
 # flask libs
 from flask import Flask
-from flask import Markup
 from flask import render_template
 from flask import send_file
-from flask import redirect
-from flask import url_for
 
 # custom libs
 from disk_data_manager import *
@@ -46,46 +43,40 @@ def index():
 
 @app.route('/disk')
 def line_disk_chart():
-    try:
-        # get disk manager singleton
-        disk_manager = get_disk_manager()
+    # get disk manager singleton
+    disk_manager = get_disk_manager()
 
-        disk = disk_manager.disks.values()[0]
+    disk = disk_manager.disks.values()[0]
 
-        last_date_named_value = disk.get_last_disk_data_value().date
-        last_disk_in_use_value = disk.get_last_disk_data_value().in_use
+    last_date_named_value = disk.get_last_disk_data_value().date
+    last_disk_in_use_value = disk.get_last_disk_data_value().in_use
 
-        disk_usage_label = "{} ({})".format(last_date_named_value, last_disk_in_use_value)
+    disk_usage_label = "{} ({})".format(last_date_named_value, last_disk_in_use_value)
 
-        return render_template(
-            'disk_chart.html',
-            line_chart_title='Server Disk Usage (Daily)',
-            disk_usage_title='Disk Usage (Current)',
-            disk_usage_label=disk_usage_label,
-            disk_usage_value=disk.get_last_disk_data_value().size,
-            disk_obj=disk
-        )
-    except Exception as ex:
-        return redirect(url_for('handle_error'))
+    return render_template(
+        'disk_chart.html',
+        line_chart_title='Server Disk Usage (Daily)',
+        disk_usage_title='Disk Usage (Current)',
+        disk_usage_label=disk_usage_label,
+        disk_usage_value=disk.get_last_disk_data_value().size,
+        disk_obj=disk
+    )
 
 
 @app.route('/download')
 def download_file():
-    try:
-        disk_manager = get_disk_manager()
-        # retrieve disk data from CSV
-        return send_file(
-            "/os-monitor/output/_dev-sdc1.csv",
-            attachment_filename='disk.csv',
-            as_attachment=True
-        )
-    except Exception as ex:
-        return redirect(url_for('handle_error'))
+    disk_manager = get_disk_manager()
+    # retrieve disk data from CSV
+    return send_file(
+        "/os-monitor/output/_dev-sdc1.csv",
+        attachment_filename='disk.csv',
+        as_attachment=True
+    )
 
 
-@app.route('/error')
-def handle_error(error_msg):
-    return render_template('error_500.html', error_msg=error_msg)
+@app.errorhandler(Exception)
+def server_error(err):
+    return render_template('error_500.html', error_msg=err)
 
 
 @app.route('/test/error/500')
