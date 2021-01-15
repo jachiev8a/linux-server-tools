403eb21e1894f52d78572a4585b22c752d1be456
Javier Ochoa <javier.ochoa@osram-continental.com>
2021-01-15 16:44:54 -0600

    improvements at crontab setup (FIX)

--- setup.sh
+++ setup.sh
@@ -174,12 +174,12 @@ chown -R "$TOOLS_USER":"$TOOLS_USER" "$TOOLS_ROOT_DIR"
 log " > [$SCRIPT_NAME]: Setup crontab for user '$TOOLS_USER'..."
 log "------------------------------------------------------------"
 
-log_debug " > [$SCRIPT_NAME]: Validate crontab file: '$CRONTAB_USER_FILE'"
+log " > [$SCRIPT_NAME]: Validate crontab file: '$CRONTAB_USER_FILE'"
 log ""
 # Crontab file exists
 if [ ! -f "$CRONTAB_USER_FILE" ] ; then
     log_debug " > [$SCRIPT_NAME]: Generating crontab file: '$CRONTAB_USER_FILE'"
-    echo "$CRONTAB_HEADER_FILE" > "$CRONTAB_USER_FILE"
+    echo -e "$CRONTAB_HEADER_FILE" > "$CRONTAB_USER_FILE"
     log_info " > [$SCRIPT_NAME]: crontab file successfully generated! [OK]"
 fi
 
@@ -187,7 +187,7 @@ CRONTAB_EXISTS_ALREADY=$( crontab -u "$TOOLS_USER" -l | grep -c "$CRONTAB_SEARCH
 
 if [ "$CRONTAB_EXISTS_ALREADY" -ne 0 ]; then
     log_debug " > Adding crontab CMD for user: '$TOOLS_USER'"
-    (crontab -u "$TOOLS_USER" -l 2>/dev/null; echo "$CRONTAB_STRING") | crontab -
+    (crontab -u "$TOOLS_USER" -l 2>/dev/null; echo -e "$CRONTAB_STRING") | crontab -
 fi
 
 log ""
