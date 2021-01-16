ea6cb6315f9128950e533b3a855c163e88e35df7
Javier Ochoa <javier.ochoa@osram-continental.com>
2021-01-15 18:57:50 -0600

    fixed major problem with setup removing root crontab, not ok (FIX)

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,6 +3,7 @@
 export SERVER_TOOLS_ROOT_DIR="/var/lib/server-tools"
 export SERVER_TOOLS_REPO_DIR="$SERVER_TOOLS_ROOT_DIR/repository"
 export SERVER_TOOLS_OS_MONITOR_OUTPUT="$SERVER_TOOLS_ROOT_DIR/os-monitor/out"
+export SERVER_TOOLS_LOGS_DIR="$SERVER_TOOLS_ROOT_DIR/logs"
 
 export SERVER_TOOLS_USER="servertool"
 
